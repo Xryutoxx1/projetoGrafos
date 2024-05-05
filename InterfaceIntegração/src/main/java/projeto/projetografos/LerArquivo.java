f19400e28ddbc78881803f2a05eb0df969637357
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-05 11:45:11 -0300

    Update LerArquivo.java

--- InterfaceIntegração/src/main/java/projeto/projetografos/LerArquivo.java
+++ InterfaceIntegração/src/main/java/projeto/projetografos/LerArquivo.java
@@ -1,28 +1,30 @@
-package projeto.projetografos;
+package projeto.projetografos; 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.nio.file.Files; // 
+import java.nio.file.Path; // 
+import java.nio.file.Paths; // Importação das classes necessárias para localizar e carregar os arquivos de entrada.
 
 /**
- *
- * @author usuario
+ * Classe responsável por ler o conteúdo de um arquivo de texto e fazer a conversão para grafo de  aresta.
  */
-
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class LerArquivo {
 
-    private String transformaEmStr;
+    private String transformaEmStr; // Variável para armazenar o conteúdo do arquivo como uma string
 
+    /**
+     * Construtor da classe LerArquivo.
+     * Lê o conteúdo do arquivo especificado e armazena em uma string.
+     * @recebe como parâmetro uma string nomeArquivo, que serve para encontrar no endereço o título do arquivo
+     * que será carregado.
+     */
     public LerArquivo(String nomeArquivo) {
+        // Cria um objeto Path com o caminho do arquivo
         Path caminho = Paths.get("/home/usuario/Área de trabalho/InstantTransmissionJframeBackground/src/main/resources/entradaTxt/" + nomeArquivo + ".txt");
         try {
+            // Lê todos os bytes do arquivo
             byte[] bytesDoArquivo = Files.readAllBytes(caminho);
+            // Converte os bytes em uma string
             transformaEmStr = new String(bytesDoArquivo);
         } catch (Exception e) {
             // Em caso de exceção, definimos transformaEmStr como uma mensagem de erro
@@ -30,7 +32,9 @@ public class LerArquivo {
         }
     }
 
-    // Método para retornar a string com o conteúdo do arquivo
+    /**
+     * Método para retornar o conteúdo do arquivo ou a mensagem de erro como uma string.
+     */
     public String getConteudoArquivo() {
         return transformaEmStr;
     }
