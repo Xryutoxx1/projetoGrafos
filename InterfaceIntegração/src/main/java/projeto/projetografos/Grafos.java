4b9e524ded620942922d316b750335dd3703d903
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-05 11:33:49 -0300

    Update Grafos.java

--- InterfaceIntegração/src/main/java/projeto/projetografos/Grafos.java
+++ InterfaceIntegração/src/main/java/projeto/projetografos/Grafos.java
@@ -1,22 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package projeto.grafos;
-
+package projeto.grafos; // Declaração do pacote onde a classe Grafos está contida
 
 /**
- *
- * @author usuario
+ * Classe principal Grafos.
+ * Responsável por iniciar a aplicação e exibir a primeira tela.
  */
 public class Grafos {
 
+    /*
+     * Método principal da aplicação.
+     * Inicializa a primeira tela representada pela classe "Tela1" e a torna visível.
+     */
     public static void main(String[] args) {
- 
+        // Cria uma instância da classe Tela1
         Tela1 minhaTela1 = new Tela1();
+        // Torna a tela visível
         minhaTela1.setVisible(true);
-        
-        
- 
     }
 }
+
